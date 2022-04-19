dd6007598b3e55c74bf47ac99423b2442d6205f5
abolfazl-hue <abolfazljdst@gmail.com>
2022-04-19 19:44:34 +0430

    add z-index to alert box

--- src/components/ToastAlert.vue
+++ src/components/ToastAlert.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast-wrapper fixed w-full top-5 text-center">
+  <div class="toast-wrapper fixed w-full top-5 text-center z-50">
     <div class="toast p-5 text-white bg-[#ff0062] rounded-lg shadow shadow-gray-900 max-w-[400px] my-0 mx-auto">
         <slot></slot>
     </div>
