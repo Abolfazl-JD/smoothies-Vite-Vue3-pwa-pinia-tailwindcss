9adf28c2f40433e269c3f30534cffd367f59ccb4
abolfazl-hue <abolfazljdst@gmail.com>
2022-04-20 13:07:12 +0430

    add switch transition for fading in and out

--- src/views/smoothiesView.vue
+++ src/views/smoothiesView.vue
@@ -6,19 +6,21 @@ const smoothiesData = appData()
 </script>
 
 <template>
-  <transition-group
-  tag="main"
-  name="list" 
-  v-if="smoothiesData.smoothies.length"
-  class="xl:w-2/3 md:w-5/6 sm:w-full sm:px-5 xs:w-2/3 w-3/4 mt-20 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 sm:gap-8 relative">
-    <!-- Smoothie item -->
-    <SmoothieItem 
-    v-for="smoothie of smoothiesData.smoothies"
-    :key="smoothie.id"
-    :smoothie="smoothie" />
+  <transition name="switch" mode="out-in">
+    <transition-group
+    v-if="smoothiesData.smoothies.length"
+    tag="main"
+    name="list" 
+    class="xl:w-2/3 md:w-5/6 sm:w-full sm:px-5 xs:w-2/3 w-3/4 mt-20 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 sm:gap-8 relative">
+      <!-- Smoothie item -->
+      <SmoothieItem 
+      v-for="smoothie of smoothiesData.smoothies"
+      :key="smoothie.id"
+      :smoothie="smoothie" />
 
-  </transition-group>
-  <h2 v-else class="font-bold text-gray-800 text-xl text-center mt-16">whooo no smoothie to drink!</h2>
+    </transition-group>
+    <h2 v-else class="font-bold text-gray-800 text-xl text-center mt-16">whooo no smoothie to drink!</h2>
+  </transition>
 </template>
 
 <style>
@@ -33,4 +35,14 @@ const smoothiesData = appData()
   transform: translateY(30px);
 }
 
+.switch-enter-from,
+.switch-leave-to{
+  opacity : 0;
+  transform: translateY(30px);
+}
+
+.switch-enter-active,
+.switch-leave-active{
+  transition: all 0.3s ease;
+}
 </style>
